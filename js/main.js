abbddce61b73d039ec2886293a192f3828d9a42d
iswyg <glushkov.nikita@instrument.ms>
2021-04-26 15:47:31 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,31 @@
+window.onresize = function(event) {
+    document.location.reload(true);
+}
+
+$(document).ready(function() {
+    $('.header__burger').click(function() {
+        $('body').toggleClass('lock');
+        $('.header__menu').toggleClass('active');
+        $('.header__burger').toggleClass('active');
+        $('.main__left').toggleClass('active');
+        $('.main__right').toggleClass('active');
+    });
+});
+if (window.screen.width <= 1024) {
+    $('.right__search').prependTo('.main__row');
+    $('#copyright').prependTo('.main__bottom');
+    console.log('1024');
+} else
+if (window.screen.width <= 1440) {
+    console.log('1440');
+    $('.right__search').prependTo('.main__row');
+
+}
+if (window.screen.width <= 767) {
+    console.log('767');
+    $('.right__search').prependTo('.main__content');
+    $('.main__title').click(function() {
+        $('.main__title').toggleClass('active');
+        $('.main__center').toggleClass('hidden');
+    });
+}
