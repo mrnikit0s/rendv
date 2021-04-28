ba1b07d6fd7db200bcb33f1efdbc14678a0f47a4
iswyg <mr_nikit0s@live.ru>
2021-04-28 20:34:53 +0300

    expert new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.header__burger').click(function() {
+$(document).ready(function () {
+    $('.header__burger').click(function () {
         $('body').toggleClass('lock');
         $('.header__menu').toggleClass('active');
         $('.header__burger').toggleClass('active');
@@ -11,14 +11,14 @@ if (window.screen.width <= 1024) {
     $('.right__search').prependTo('.main__row');
     $('#copyright').prependTo('.main__bottom');
 } else
-if (window.screen.width <= 1440) {
-    $('.right__search').prependTo('.main__row');
+    if (window.screen.width <= 1440) {
+        $('.right__search').prependTo('.main__row');
 
-}
+    }
 if (window.screen.width <= 767) {
     console.log('767');
     $('.right__search').prependTo('.main__content');
-    $('.main__title').click(function() {
+    $('.main__title').click(function () {
         $('.main__title').toggleClass('active');
         $('.main__center').toggleClass('hidden');
     });
